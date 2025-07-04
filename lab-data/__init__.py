f2e214f9a0d8b447177d8551e4fdf90c9c7f69e1
RPope-TechAdmin <rpope@purenv.au>
2025-07-04 12:38:38 +1000

    Changing cors_headers to allow proper access in test app

--- lab-data/__init__.py
+++ lab-data/__init__.py
@@ -1,4 +1,12 @@
 import azure.functions as func
 
+cors_headers= {
+    "Access-Control-Allow-Origin": "https://delightful-tree-0888c340f.1.azurestaticapps.net",
+    "Access-Control-Allow-Methods": "POST, OPTIONS, GET",
+    "Access-Control-Allow-Headers": "Content-Type, Accept",
+    "Access-Control-Max-Age": "86400"
+}
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("Function reached successfully.", status_code=200)
+    if req.method=="OPTIONS":
+        return func.HttpResponse("Function reached successfully.", status_code=200, headers=cors_headers)
